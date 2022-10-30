b502d04ab577fb3dd63b3dd9d956ee5fb69e6a68
naveenbavu <naveenbavu@gmail.com>
2022-10-29 21:48:50 -0500

    commiting the project structure

---
+++ src/main/java/com/example/employeeproj/EmployeeprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeprojApplication.class, args);
+	}
+
+}
